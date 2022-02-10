e7770cd8c9d21ad6794985273cd2a6649d49f4d4
Oleg Tarasenko <oleg.tarasenko@devit.group>
2022-02-10 14:16:01 +0200

    fi43

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const PORT = 80;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
